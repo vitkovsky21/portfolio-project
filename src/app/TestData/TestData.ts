27799da699eeccaae4a6b5f852790face9ad5381
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-02-21 20:47:49 +0300

    portfolio updated part 3

--- src/app/TestData/TestData.ts
+++ src/app/TestData/TestData.ts
@@ -4,9 +4,10 @@ export class TestData {
 
     static sites: Site[] = [
         {id: 1, image: "../../assets/images/img-1.jpeg", title: "CoffeeShop Website", link: 'https://portfolio-coffeeshop.web.app/', github: "https://github.com/vitkovsky21/coffee-shop-angular-css-bootstrap"},
-        {id: 2, image: "../../assets/images/img-2.jpg", title: 'Shopping Website(using client-server communication)', link: 'https://portfolio-shoppingwebsite.web.app', github: "https://github.com/vitkovsky21/shopping-magazine"},
-        {id: 3, image: "../../assets/images/img-3.jpg", title: 'TODO Website (simple TODO project using bootstrap)', link: 'https://portfolio-simpletodo.web.app/', github: "https://github.com/vitkovsky21/todo-angular"},
-        {id: 4, image: "../../assets/images/img-4.jpg", title: 'Course Assignment Project (using Angular Material and Heroku)', link: 'https://portfolio-courseassignment.web.app', github: "https://github.com/vitkovsky21/conFusion"}
+        {id: 2, image: "../../assets/images/img-2.jpg", title: 'Shopping Website using client-server communication', link: 'https://portfolio-shoppingwebsite.web.app', github: "https://github.com/vitkovsky21/shopping-magazine"},
+        {id: 3, image: "../../assets/images/img-5.jpg", title: 'My Portfolio Website Itself :)', link: 'https://portfolio-vladislav.web.app/', github: "#"},
+        {id: 4, image: "../../assets/images/img-3.jpg", title: 'TODO Website (simple TODO project using bootstrap)', link: 'https://portfolio-simpletodo.web.app/', github: "https://github.com/vitkovsky21/todo-angular"},
+        {id: 5, image: "../../assets/images/img-4.jpg", title: 'Course Assignment Project using Angular Material and Heroku', link: 'https://portfolio-courseassignment.web.app', github: "https://github.com/vitkovsky21/conFusion"}
     ]
 
 }
