6de53f10b387a9683b5be24cdcfef2a1bf376c93
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-02-21 20:38:02 +0300

    portolio updated part 2

--- src/app/TestData/TestData.ts
+++ src/app/TestData/TestData.ts
@@ -4,8 +4,9 @@ export class TestData {
 
     static sites: Site[] = [
         {id: 1, image: "../../assets/images/img-1.jpeg", title: "CoffeeShop Website", link: 'https://portfolio-coffeeshop.web.app/', github: "https://github.com/vitkovsky21/coffee-shop-angular-css-bootstrap"},
-        {id: 2, image: "../../assets/images/img-2.jpg", title: 'Shopping Website', link: 'https://portfolio-shoppingwebsite.web.app', github: "https://github.com/vitkovsky21/shopping-magazine"},
-        {id: 3, image: "../../assets/images/img-3.jpg", title: 'TODO Website', link: 'https://portfolio-simpletodo.web.app/', github: "https://github.com/vitkovsky21/todo-angular"}
+        {id: 2, image: "../../assets/images/img-2.jpg", title: 'Shopping Website(using client-server communication)', link: 'https://portfolio-shoppingwebsite.web.app', github: "https://github.com/vitkovsky21/shopping-magazine"},
+        {id: 3, image: "../../assets/images/img-3.jpg", title: 'TODO Website (simple TODO project using bootstrap)', link: 'https://portfolio-simpletodo.web.app/', github: "https://github.com/vitkovsky21/todo-angular"},
+        {id: 4, image: "../../assets/images/img-4.jpg", title: 'Course Assignment Project (using Angular Material and Heroku)', link: 'https://portfolio-courseassignment.web.app', github: "https://github.com/vitkovsky21/conFusion"}
     ]
 
 }
