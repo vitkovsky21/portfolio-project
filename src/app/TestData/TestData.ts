f988a229787ddce2db38ff7f10c87fcbf987ad15
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-08-11 16:34:23 +0300

    added new portfolio websites

--- src/app/TestData/TestData.ts
+++ src/app/TestData/TestData.ts
@@ -3,11 +3,14 @@ import { Site } from "../class/site";
 export class TestData {
 
     static sites: Site[] = [
-        {id: 1, image: "../../assets/images/img-1.jpeg", title: "CoffeeShop Website", link: 'https://portfolio-coffeeshop.web.app/', github: "https://github.com/vitkovsky21/coffee-shop-angular-css-bootstrap"},
-        {id: 2, image: "../../assets/images/img-2.jpg", title: 'Shopping Website', link: 'https://portfolio-shoppingwebsite.web.app', github: "https://github.com/vitkovsky21/shopping-magazine"},
-        {id: 3, image: "../../assets/images/img-5.jpg", title: 'My Portfolio Website Itself :)', link: 'https://portfolio-b-vladislav.web.app/', github: "https://github.com/vitkovsky21/portfolio-project"},
-        {id: 4, image: "../../assets/images/img-3.jpg", title: 'TODO Table', link: 'https://portfolio-simpletodo.web.app/', github: "https://github.com/vitkovsky21/todo-angular"},
-        {id: 5, image: "../../assets/images/img-4.jpg", title: 'Course Assignment Project', link: 'https://portfolio-courseassignment.web.app', github: "https://github.com/vitkovsky21/conFusion"}
+        {id: 1, image: "../../assets/images/img-7.bmp", title: "Parfumdelame Shop", link: 'https://parfumdelame-sample3.web.app/', github: "https://github.com/vitkovsky21/parfumdelame"},
+        {id: 2, image: "../../assets/images/img-1.jpeg", title: "CoffeeShop Website", link: 'https://portfolio-coffeeshop.web.app/', github: "https://github.com/vitkovsky21/coffee-shop-angular-css-bootstrap"},
+        {id: 3, image: "../../assets/images/img-9.jpeg", title: "Vue.js Shop", link: 'https://62f3d5c9fa4c5d009f9cdbbc--strong-cuchufli-53c054.netlify.app/', github: "https://github.com/vitkovsky21/vue-shop"},
+        {id: 4, image: "../../assets/images/img-2.jpg", title: 'Shopping Website', link: 'https://portfolio-shoppingwebsite.web.app', github: "https://github.com/vitkovsky21/shopping-magazine"},
+        {id: 5, image: "../../assets/images/img-5.jpg", title: 'My Portfolio Website Itself :)', link: 'https://portfolio-b-vladislav.web.app/', github: "https://github.com/vitkovsky21/portfolio-project"},
+        {id: 6, image: "../../assets/images/img-3.jpg", title: 'TODO Table', link: 'https://portfolio-simpletodo.web.app/', github: "https://github.com/vitkovsky21/todo-angular"},
+        {id: 7, image: "../../assets/images/img-8.jpg", title: 'Messenger (in progress)', link: 'https://github.com/vitkovsky21/middle.messenger.praktikum.yandex/tree/sprint_3', github: 'https://github.com/vitkovsky21/middle.messenger.praktikum.yandex/tree/sprint_3'},
+        {id: 8, image: "../../assets/images/img-4.jpg", title: 'Course Assignment Project', link: 'https://portfolio-courseassignment.web.app', github: "https://github.com/vitkovsky21/conFusion"}
     ]
 
 }
